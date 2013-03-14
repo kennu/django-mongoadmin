23efd713c7a56149ca5ecbfd3b408f443fc0a835
Kenneth Falck <kennu@iki.fi>
2013-03-14 19:52:01 +0200

    Fix LOOKUP_SEP import

--- mongoadmin/options.py
+++ mongoadmin/options.py
@@ -13,7 +13,7 @@ from django.db.models.related import RelatedObject
 from django.db.models.fields import BLANK_CHOICE_DASH, FieldDoesNotExist
 try: from django.db.models.sql.constants import LOOKUP_SEP
 except ImportError: from django.db.models.constants import LOOKUP_SEP
-from django.db.models.sql.constants import LOOKUP_SEP, QUERY_TERMS
+from django.db.models.sql.constants import QUERY_TERMS
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.utils.decorators import method_decorator
